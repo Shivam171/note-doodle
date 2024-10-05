9cd7fa333931e624a098a84db17317170056f28c
Shivam171 <shiv.op@gmail.com>
2024-10-05 21:34:33 +0530

    Convex Alternate Query

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,34 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
-
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex, useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+    const { user }: any = useKindeBrowserClient();
+    const convex = useConvex();
+    const createUser = useMutation(api.user.createUser)
+
+    useEffect(() => {
+        if (user) {
+            checkUser();
+        }
+    }, [user])
+
+    const checkUser = async () => {
+        const result = await convex.query(api.user.getUser, {
+            email: user?.email
+        })
+        if (!result?.length) {
+            createUser({
+                name: user.given_name,
+                email: user.email,
+                image: user.picture
+            }).then((res) => console.log(res))
+        }
+    }
+
     return (
         <div>
             <h1>Dashboard</h1>
