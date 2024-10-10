e31da844b10f9c48104cb89cf99264bd30b2ec50
Shivam171 <shiv.op@gmail.com>
2024-10-10 15:52:01 +0530

    Save and Open Canvas

--- app/(routes)/workspace/[id]/page.tsx
+++ app/(routes)/workspace/[id]/page.tsx
@@ -1,52 +1,91 @@
 "use client"
-import WorkspaceHeader from "../_components/WorkspaceHeader"
-import Editor from "../_components/Editor"
-import { useEffect, useState } from "react"
+import WorkspaceHeader from "../_components/WorkspaceHeader";
+import Editor from "../_components/Editor";
+import { useEffect, useState } from "react";
 import { useConvex } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import Canvas from "../_components/Canvas";
 
 export interface FILE {
-    archive: boolean,
-    createdBy: string,
-    document: string,
-    fileName: string,
-    _id: string,
-    teamId: string,
-    updatedAt: string,
-    whiteboard: string,
-    _creationTime: number
+    archive: boolean;
+    createdBy: string;
+    document: string;
+    fileName: string;
+    _id: string;
+    teamId: string;
+    updatedAt: string;
+    whiteboard: string;
+    _creationTime: number;
 }
 
 export default function Workspace({ params }: any) {
     const [triggerSave, setTriggerSave] = useState(false);
     const [fileData, setFileData] = useState<FILE | any>();
+    const [leftWidth, setLeftWidth] = useState(50);
+    const [isDragging, setIsDragging] = useState(false);
     const convex = useConvex();
 
     useEffect(() => {
-        params.id && getFileData()
-    }, [])
+        params.id && getFileData();
+    }, []);
 
     const getFileData = async () => {
         const result = await convex.query(api.files.getFileById, {
-            _id: params.id
-        })
+            _id: params.id,
+        });
         setFileData(result);
-    }
+    };
+
+    const handleMouseDown = () => {
+        setIsDragging(true);
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+        if (!isDragging) return;
+        const newWidth = (e.clientX / window.innerWidth) * 100;
+        setLeftWidth(newWidth > 10 && newWidth < 90 ? newWidth : leftWidth);
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
+    useEffect(() => {
+        if (isDragging) {
+            window.addEventListener("mousemove", handleMouseMove);
+            window.addEventListener("mouseup", handleMouseUp);
+        } else {
+            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mouseup", handleMouseUp);
+        }
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mouseup", handleMouseUp);
+        };
+    }, [isDragging]);
 
     return (
         <div className="flex flex-col h-screen w-full">
             <WorkspaceHeader onSave={() => setTriggerSave(!triggerSave)} fileId={params.id} />
             {/* Workspace Layout */}
-            <div className="grid grid-cols-1 md:grid-cols-2 h-full p-6">
+            <div className="flex h-full px-6">
                 {/* Document */}
-                <div className="">
+                <div className="relative" style={{ width: `${leftWidth}%` }}>
                     <Editor onSaveTrigger={triggerSave} fileId={params.id} fileData={fileData} />
                 </div>
+
+                {/* Draggable Divider */}
+                <div
+                    className="w-1 bg-gray-100 cursor-col-resize"
+                    onMouseDown={handleMouseDown}
+                ></div>
+
                 {/* Whiteboard/Canvas */}
-                <div className="">
-                    Whiteboard
+                <div className="border-l" style={{ width: `${100 - leftWidth}%` }}>
+                    <Canvas onSaveTrigger={triggerSave} fileId={params.id} fileData={fileData} />
                 </div>
             </div>
         </div>
-    )
+    );
 }
---
+++ app/(routes)/workspace/_components/Canvas.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Excalidraw, WelcomeScreen, MainMenu } from "@excalidraw/excalidraw";
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+
+export default function Canvas({ onSaveTrigger, fileId, fileData }: any) {
+    const [whiteBoardData, setWhiteBoardData] = useState<any>();
+    const updateWhiteboard = useMutation(api.files.updateWhiteboard);
+
+    useEffect(() => {
+        onSaveTrigger && saveWhiteboard();
+    }, [onSaveTrigger])
+
+    const saveWhiteboard = async () => {
+        updateWhiteboard({
+            _id: fileId,
+            whiteboard: JSON.stringify(whiteBoardData),
+        }).then((res) => {
+            // console.log(res);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    return (
+        <div className='w-full h-full'>
+            <div className='h-full'>
+                {fileData &&
+                    <Excalidraw
+                        theme='light'
+                        initialData={{
+                            elements: fileData?.whiteboard && JSON.parse(fileData?.whiteboard)
+                        }}
+                        onChange={(excalidrawElements, appState, files) => {
+                            setWhiteBoardData(excalidrawElements);
+                        }}
+                        UIOptions={{
+                            canvasActions: {
+                                saveToActiveFile: false,
+                                loadScene: false,
+                                export: false,
+                                toggleTheme: false
+                            }
+                        }}
+                    >
+                        <MainMenu>
+                            <MainMenu.DefaultItems.ClearCanvas />
+                            <MainMenu.DefaultItems.SaveAsImage />
+                            <MainMenu.DefaultItems.ChangeCanvasBackground />
+                        </MainMenu>
+
+                        <WelcomeScreen>
+                            <WelcomeScreen.Hints.ToolbarHint>
+                                <p> ToolBar Area </p>
+                            </WelcomeScreen.Hints.ToolbarHint>
+                            <WelcomeScreen.Hints.MenuHint />
+                            <WelcomeScreen.Hints.HelpHint />
+                            <WelcomeScreen.Center>
+                                <WelcomeScreen.Center.Logo>
+                                    <Image src="/logo.png" loading='lazy' alt="logo" width={300} height={300} />
+                                </WelcomeScreen.Center.Logo>
+                                <WelcomeScreen.Center.Heading>
+                                    Start bringing your ideas to life
+                                </WelcomeScreen.Center.Heading>
+                            </WelcomeScreen.Center>
+                        </WelcomeScreen>
+                    </Excalidraw>
+                }
+            </div>
+        </div>
+    )
+}
